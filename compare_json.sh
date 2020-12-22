65471725bef0a2beeba946bc525f18bf49ac6156
ezamora <zr.esteban@gmail.com>
2020-12-22 20:54:51 -0300

    Testing in a real situation

--- compare_json.sh
+++ compare_json.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-./convert_xml.pl $1".xml" > $1".json"
-./convert_xml.pl $2".xml" > $2".json"
+LPATH=$HOME/Dropbox/Projects/perlScripts/XMLCOMPARATOR
+
+$LPATH/convert_xml.pl $1".xml" > $1".json"
+$LPATH/convert_xml.pl $2".xml" > $2".json"
 
 cond=$(jq --argfile a $1.json --argfile b $2.json -n 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); ($a | (post_recurse | arrays) |= sort) as $a | ($b | (post_recurse | arrays) |= sort) as $b | $a == $b')
 
